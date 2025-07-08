f80e5bebeb1f34078b151524bcaa2b295ad02132
Hazel Mito <hazelmito2029@gmail.com>
2025-07-08 18:48:10 +0800

    feat(bootstrap.php): Added define utils path

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
 define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
+define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
 
 chdir(BASE_PATH);
 
