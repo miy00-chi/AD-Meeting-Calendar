a80f0acc2d46c64673ed6ba82466a3b889bd10bd
Hazel Mito <hazelmito2029@gmail.com>
2025-07-08 21:39:54 +0800

    feat(bootstrap.php): Added Define database and dummies path

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,9 @@
 define('BASE_PATH', realpath(__DIR__));
 define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
 define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
+define('DATABASE_PATH', realpath(BASE_PATH . "/database"));
+define('DUMMIES_PATH', realpath(BASE_PATH . "/staticDatas/dummies"));
+
 
 chdir(BASE_PATH);
 
