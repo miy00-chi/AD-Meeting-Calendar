1114d3dad83a5a4862015a466f41b7d6bacafaa8
Hazel Mito <hazelmito2029@gmail.com>
2025-07-08 19:43:59 +0800

    feat(dbResetPostgresql.util.php): Added dbResetPostgresql.util.php and set up requirements

---
+++ utils/dbResetPostgresql.util.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+// 1) Composer autoload
+require_once 'vendor/autoload.php';
+
+// 2) Composer bootstrap
+require_once 'bootstrap.php';
+
+// 3) envSetter
+require_once UTILS_PATH . '/envSetter.util.php';
+
+
+
+?>
