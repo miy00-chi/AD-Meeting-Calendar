82b3d29b200cd937314f5ac0f08bc424a57089d8
Hazel Mito <hazelmito2029@gmail.com>
2025-07-08 18:15:24 +0800

    refactor(posgreChecker.handler.php): Updated information in port and dbname to avoid connection fail

--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -1,10 +1,10 @@
 <?php
 
 $host = "host.docker.internal"; 
-$port = "5112";
+$port = "5113";
 $username = "user";
 $password = "password";
-$dbname = "mydatabase";
+$dbname = "meetingCalendar";
 
 $conn_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
 
